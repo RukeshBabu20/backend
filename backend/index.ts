4baa1c25b6c2b264431aeadddf75b5803c83f5b0
RukeshBabu20 <rukeshbabu20@gmail.com>
2023-11-30 01:23:53 +0530

    first commit

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import dbConnection from "./database/dbConnection";
 import employeeRoute from "./routes/employeeRoute";
-import studentRoute from "./routes/studentRoutes";
 import cors from "cors";
 const app = express();
 const PORT = 5000;
@@ -12,7 +11,6 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/employee", employeeRoute);
-app.use("/student", studentRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
--- models/studentModel.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const studentSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-});
